580044b8f66a1cc47bf6b3947c7966cfe10a98bc
Griss Garcia <grissgarcia74@gmail.com>
2018-07-07 17:22:40 -0400

    First layout implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokedexComponent
   ],
   imports: [
     BrowserModule
