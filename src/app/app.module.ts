cfecb8678ee336f9f7ba7e61832d6301cd75770b
Griss Garcia <grissgarcia74@gmail.com>
2018-07-07 20:39:11 -0400

    Third layout implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PokedexComponent } from './pokedex/pokedex.component';
+import { PokedexService } from './pokedex.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,12 @@ import { PokedexComponent } from './pokedex/pokedex.component';
     PokedexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    PokedexService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from '../../../node_modules/rxjs';
+import { PokedexService } from 'src/app/pokedex.service';
 
 @Component({
   selector: 'app-pokedex',
@@ -6,69 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokedex.component.css']
 })
 export class PokedexComponent {
-  pokemonList: Array<any>;
+  pokemonList: Observable<Array<any>>;
 
   selectedPokemon: any;
 
-  constructor() {
-    this.pokemonList = [
-      {
-        number: 1,
-        name: 'Bulbasaur',
-        imgURL:
-          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
-      },
-      {
-        number: 2,
-        name: 'Ivysaur',
-        imgURL:
-          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png'
-      },
-      {
-        number: 3,
-        name: 'Venusaur',
-        imgURL:
-          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png'
-      },
-      {
-        number: 4,
-        name: 'Charmander',
-        imgURL:
-          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'
-      },
-      {
-        number: 5,
-        name: 'Charmeleon',
-        imgURL:
-          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png'
-      },
-      {
-        number: 6,
-        name: 'Charizard',
-        imgURL:
-          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png'
-      },
-      {
-        number: 7,
-        name: 'Squirtle',
-        imgURL:
-          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png'
-      },
-      {
-        number: 8,
-        name: 'Wartortle',
-        imgURL:
-          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png'
-      },
-      {
-        number: 9,
-        name: 'Blastoise',
-        imgURL:
-          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png'
-      }
-    ];
+  constructor(private pokedexService: PokedexService) {
+    this.pokemonList = this.pokedexService.getPokemons();
 
-    this.selectedPokemon = this.pokemonList[0];
+    this.selectedPokemon = {
+      number: 0,
+      name: '',
+      imgURL: ''
+    };
   }
 
   setSelectedPokemon(pokemon): void {
